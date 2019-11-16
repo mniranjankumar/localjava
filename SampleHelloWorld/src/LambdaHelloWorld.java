addc4f21fd3217a4867fdd433bc77f3d2a06ebe0
niranjan <niranjan.mnk.07@gmail.com>
2019-11-16 15:25:46 +0530

    Revert "commit"
    This reverts commit 9ad8c4126ab6653c0d541cfc9a382299ed6a5c2a.

---
+++ SampleHelloWorld/src/LambdaHelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaHelloWorld {
+	interface HelloWorld {
+		String helo(String name);
+	}
+	
+	public static void main(String[] args) {
+		List l = new ArrayList();
+	}
+
+}
